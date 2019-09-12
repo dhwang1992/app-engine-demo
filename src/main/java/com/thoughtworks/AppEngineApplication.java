b5650c00c8cb02c86f4219ac7419ba984ae2ceab
dhwang <dhwang@thoughtworks.com>
2019-09-12 17:31:08 +0800

    first init

---
+++ src/main/java/com/thoughtworks/AppEngineApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppEngineApplication.class, args);
+    }
+}
